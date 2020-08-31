0406c355fd8246eb4b002eee6fe8d33baeca8709
PWu-gh <pierre.wu@edu.univ-paris13.fr>
2020-08-31 08:53:44 +0200

    ready for heroku

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,5 +1,5 @@
 // const socket = io.connect('http://localhost:8000');
-const socket = io.connect('https://quickmafs/herokuapp.com');
+const socket = io.connect('https://quickmafs.herokuapp.com');
 const msgDisplay = document.getElementById('display_chat');
 const msgInput = document.getElementById('message');
 const msgForm = document.getElementById('send_msg');
